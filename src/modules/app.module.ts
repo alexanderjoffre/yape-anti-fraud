669c142334b7114ffb697691aff4b9bbf7cff12c
Alexander Joffre Aguirre <alexanderjoffreaguirre@Alexanders-Mac-mini.local>
2024-10-09 12:07:24 -0500

    feat: mongoose ODM added

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/yape-anti-fraud'),
+  ],
 })
 export class AppModule {}
